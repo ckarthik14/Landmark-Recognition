54a0624a64e68d7d66ba62bf5619f86680fd9b3b
ckarthik14 <ckarthik114@gmail.com>
2019-09-14 11:48:11 +0530

    Fixed test downloaded script

--- Bounding_Box_Scripts/CSV_Generators/test_downloaded.py
+++ Bounding_Box_Scripts/CSV_Generators/test_downloaded.py
@@ -7,7 +7,7 @@ import csv
 from utils import get_dict
 
 # Creates dictionary to obtain landmark id from train_dict and bounding boxes from box_dict
-train_dict = get_dict("train.csv", 2)
+test_dict = get_dict("test.csv", 2)
 box_dict = get_dict("boxes_split2.csv", 1)
 
 with open('test_downloaded.csv', 'w', newline='') as csvfile:
